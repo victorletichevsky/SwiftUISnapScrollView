15f73afbd9071da922e40e73592edefc5f4546a8
victorletichevsky <victorletichevsky@gmail.com>
2022-11-11 22:41:26 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SnapScrollView
+
+A description of this package.
---
+++ Tests/SnapScrollViewTests/SnapScrollViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SnapScrollView
+
+final class SnapScrollViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SnapScrollView().text, "Hello, World!")
+    }
+}
